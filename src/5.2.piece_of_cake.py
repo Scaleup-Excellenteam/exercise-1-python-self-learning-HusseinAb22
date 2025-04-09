0187b817aced8e0b229dee9b021fd3d38cccebb0
HusseinAb22 <hussein.ab222@gmail.com>
2025-04-09 17:57:42 +0300

    Update 5.2.piece_of_cake.py

--- src/5.2.piece_of_cake.py
+++ src/5.2.piece_of_cake.py
@@ -20,7 +20,7 @@ def piece_of_cake(prices, optionals=None, **bought_items):
     checkout = 0
     for item, grams in bought_items.items():
         if item not in optionals:
-            checkout += int((grams / 100) * prices.get(item, 0))
+            checkout += int((grams / 100) * prices.get(item))
     return checkout
 
 
