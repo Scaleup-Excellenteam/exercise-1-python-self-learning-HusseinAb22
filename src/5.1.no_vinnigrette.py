a40c12d58e139b761e3e2fc047dde522c3a280a9
HusseinAb22 <hussein.ab222@gmail.com>
2025-04-10 11:31:32 +0300

    Updatev2 5.1.no_vinnigrette.py

--- src/5.1.no_vinnigrette.py
+++ src/5.1.no_vinnigrette.py
@@ -7,21 +7,17 @@ import random as rnd
 import datetime
 
 
-def no_vinnigrete():
+def no_vinnigrete(input_date1,input_date2):
     """
     Generates a random date between two given dates and checks if it falls on a Monday.
+    Args:
+        input_date1 (str): The first date in YYYY-MM-DD format.
+        input_date2 (str): The second date in YYYY-MM-DD format.
     Prints:
         A message if the generated random date is not a Monday.
     """
-    while True:
-        try:
-            input_date1 = input("Enter the first date (YYYY-MM-DD): ")
-            input_date2 = input("Enter the second date (YYYY-MM-DD): ")
-            date1 = datetime.datetime.strptime(input_date1, "%Y-%m-%d")
-            date2 = datetime.datetime.strptime(input_date2, "%Y-%m-%d")
-            break  
-        except ValueError:
-            print("Invalid date format. Please use YYYY-MM-DD.\n")
+    date1 = datetime.datetime.strptime(input_date1, "%Y-%m-%d")
+    date2 = datetime.datetime.strptime(input_date2, "%Y-%m-%d")
     epoch1 = int(date1.timestamp())
     epoch2 = int(date2.timestamp())
     if epoch1 > epoch2:
@@ -32,5 +28,25 @@ def no_vinnigrete():
         print("Ain't gettin' no vinaigrette today :(")
 
 
+def valid_date():
+    """
+     Prompts the user to input two dates and validates their format.
+    Repeatedly asks for input until both dates are in valid YYYY-MM-DD format.
+    Once valid dates are provided, it calls the `no_vinnigrete` function to
+    generate a random date and print a message based on whether it falls on a Monday.
+    Exceptions:
+        Catches and handles ValueError if input is not a valid date format.
+    """
+    while True:
+        try:
+            input1 = input("Enter the first date (YYYY-MM-DD): ")
+            input2 = input("Enter the second date (YYYY-MM-DD): ")
+            no_vinnigrete(input1, input2)
+            break
+        except ValueError:
+            print("Invalid date format. Please use YYYY-MM-DD.\n")
+
+
 if __name__ == '__main__':
-    no_vinnigrete()
+    valid_date()
+
